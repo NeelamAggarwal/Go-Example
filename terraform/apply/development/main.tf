4781c5b9edd6390c3d30f71f2e64524d67f65a67
NeelamAggarwal <neelamsept@gmail.com>
2018-05-23 11:31:09 -0700

    changing path in terrform to find the zip

--- terraform/apply/development/main.tf
+++ terraform/apply/development/main.tf
@@ -14,8 +14,8 @@ resource "aws_lambda_function" "demo_lambda" {
     function_name = "demo_lambda"
     handler = "demo.handler"
     runtime = "go1.x"
-    filename = "demo.zip"
-    source_code_hash = "${base64sha256(file("demo.zip"))}"
+    filename = "../../../demo.zip"
+    source_code_hash = "${base64sha256(file('../../../demo.zip'))}"
 }
 
 
